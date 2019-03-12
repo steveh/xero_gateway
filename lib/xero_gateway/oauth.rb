6a87f9ddd28d09a45969f69d540c14215e29d8e7
Steve Hoeksema <steve@kotiri.com>
2019-03-13 10:18:39 +1300

    Capture full error details with OAuth exceptions

--- lib/xero_gateway/oauth.rb
+++ lib/xero_gateway/oauth.rb
@@ -7,11 +7,19 @@ module XeroGateway
   # http://github.com/jnunemaker/twitter/
 
   class OAuth
+    class Error < StandardError
+      def initialize(message, error_details)
+        super(message)
 
-    class TokenExpired < StandardError; end
-    class TokenInvalid < StandardError; end
-    class RateLimitExceeded < StandardError; end
-    class UnknownError < StandardError; end
+        @error_details = error_details
+      end
+
+      attr_reader :error_details
+    end
+    class TokenExpired < Error; end
+    class TokenInvalid < Error; end
+    class RateLimitExceeded < Error; end
+    class UnknownError < Error; end
 
     unless defined? XERO_CONSUMER_OPTIONS
       XERO_CONSUMER_OPTIONS = {
@@ -27,7 +35,7 @@ module XeroGateway
 
     def initialize(ctoken, csecret, options = {})
       @ctoken, @csecret = ctoken, csecret
-      
+
       # Allow user-agent base val for certification procedure (enforce for PartnerApp)
       @base_headers = {}
       @base_headers["User-Agent"] = options.delete(:user_agent) if options.has_key?(:user_agent)
@@ -40,14 +48,14 @@ module XeroGateway
     end
 
     def request_token(params = {})
-      # Underlying oauth consumer accepts body params and headers for request via positional params - explicit nilling of 
+      # Underlying oauth consumer accepts body params and headers for request via positional params - explicit nilling of
       #  body parameters allows for correct position for headers
       @request_token ||= consumer.get_request_token(params, nil, @base_headers)
     end
 
     def authorize_from_request(rtoken, rsecret, params = {})
       request_token     = ::OAuth::RequestToken.new(consumer, rtoken, rsecret)
-      # Underlying oauth consumer accepts body params and headers for request via positional params - explicit nilling of 
+      # Underlying oauth consumer accepts body params and headers for request via positional params - explicit nilling of
       #  body parameters allows for correct position for headers
       access_token      = request_token.get_access_token(params, nil, @base_headers)
       @atoken, @asecret = access_token.token, access_token.secret
@@ -71,7 +79,7 @@ module XeroGateway
 
       old_token = ::OAuth::RequestToken.new(consumer, access_token, access_secret)
 
-      # Underlying oauth consumer accepts body params and headers for request via positional params - explicit nilling of 
+      # Underlying oauth consumer accepts body params and headers for request via positional params - explicit nilling of
       #  body parameters allows for correct position for headers
       access_token = old_token.get_access_token({
         :oauth_session_handle => session_handle,
@@ -89,15 +97,15 @@ module XeroGateway
     def get(path, headers = {})
       access_token.get(path, headers.merge(@base_headers))
     end
-      
+
     def post(path, body = '', headers = {})
       access_token.post(path, body, headers.merge(@base_headers))
     end
-      
+
     def put(path, body = '', headers = {})
       access_token.put(path, body, headers.merge(@base_headers))
     end
-    
+
     def delete(path, headers = {})
       access_token.delete(path, headers.merge(@base_headers))
     end
